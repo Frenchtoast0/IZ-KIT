39f03a25ca2fb58ba4b4c1bae9821b577b025425
Frenchtoast0 <38544250+Frenchtoast0@users.noreply.github.com>
2021-02-10 10:20:59 -0700

    Working on wifi stuff

--- NodeCode/src/main.cpp
+++ NodeCode/src/main.cpp
@@ -1,50 +1,55 @@
 #include <Arduino.h>
-#include "ESP8266WiFi.h"
+#include "ESP8266WiFi.h" 
+#include "ESP8266HTTPClient.h"
 #include "pinMaps.h"
 
-//copied from: https://www.arduino.cc/en/Reference/WiFiClient
+const char* ssid = "NodeMCU";  //wifi SSID
+const char* pass = "passverd"; //wifi password
 
-char ssid[] = "NodeMCU";          //  your network SSID (name)
-char pass[] = "passverd";   // your network password
 
-int status = WL_IDLE_STATUS;
-//IPAddress server(3,211,11,78); //http request dumper
+const char* server = "google.ca";//"thor.net.nait.ca"; //server of node control
+const int port = 80;//443; //port on server, 443=https, 80=http
 
-char server[] = "httpbin.org";
-
-// Initialize the client library
-WiFiClient client;
+WiFiClient client; //wifi connection holder
 
 void setup() {
+  //start serial communications
   Serial.begin(115200);
-  Serial.println("Attempting to connect to WPA network...");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
 
+  //start wifi connection
+  Serial.println("Attempting to connect to Wifi network...");
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
+  Serial.println("Connected to wifi");
 
-    Serial.println("Connected to wifi");
-    Serial.println("\nStarting connection...");
-    // if you get a connection, report back via serial:
-    if (client.connect(server, 80)) {
-      Serial.println("connected");
-      // Make a HTTP request:
-      client.println("GET /anything HTTPS/1.0");
-      client.println();
-
-      Serial.println(client.readString());
-    }
-    else
-    {
-        Serial.println("Failed...");
-    }
+  //attempt to contact server
+  Serial.println("Starting server connection...");
+  
+  if (client.connect(server, port))
+  {
+    Serial.println("Connected!");
     
+
+    //String req = "GET /~izkit/httpTest.php HTTP/1.0\r\n";
+    //req += "Host: thor.net.nait.ca";
+    client.print("GET /search?sxsrf=ALeKk01y2phPwZbEUrsEXU4nLFGdoy5qbw%3A1612918277669&ei=BS4jYOCzKJHn-gSgwIWIBg&q=hello&oq=hello&gs_lcp=CgZwc3ktYWIQAzIKCC4QsQMQQxCTAjIECAAQQzIECC4QQzIECC4QQzIECC4QQzIECAAQQzIECAAQQzIFCAAQsQMyBAgAEEMyBQgAELEDOgQIIxAnOgoILhCxAxCDARBDULUWWPwbYN0daABwAHgAgAG9AYgB9ASSAQM1LjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab&ved=0ahUKEwigyvfnjN7uAhWRs54KHSBgAWEQ4dUDCA0&uact=5 HTTP/1.0\r\n\r\n");
+    //client.print("GET /~izkit/httpTest.php HTTP/1.1\r\n");
+    //client.print("Host: thor.net.nait.ca\r\n\r\n");
+    //make http request
+    //client.print("GET /~izkit/httpTest.php HTTP/1.1\r\n");
+    //client.print("Host: thor.net.nait.ca\r\n");
+    //client.print("GET / HTTP/1.1\r\n");
+    //client.print("Host: www.website.org\r\n");
+    //Serial.println(client.write())
+    //display response
+    Serial.println(client.readString());
+  }
+  else Serial.println("Connection failed!");
 }
 
 void loop() {
-    delay(10);
+  delay(10);
 }
